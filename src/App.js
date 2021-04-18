48b4db5a6a9a8dc92fa38dcd95645856f1148ce9
A-Little-Hat <soumyasarkar309@gmail.com>
2021-04-18 20:58:54 +0530

    Footer TodoList Todo component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import Header from './components/Header'
+import Todos from './components/Todos'
+import Footer from './components/Footer'
+
 function App() {
+  let todos = [
+    {
+      no:0,
+      title:"eat"
+    },
+    {
+      no:1,
+      title:"sleep"
+    },
+    {
+      no:2,
+      title:"code"
+    }
+  ]
   return (
     <div className="App">
       <Header/>
+      <Todos/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Todos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import TodoList from './TodoList'
+
+export default function Todos() {
+    return (
+        <div className="container">
+            <h3>Todos List</h3>
+            {}
+        </div>
+    )
+}
