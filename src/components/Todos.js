983a7a7d492f8c13584445ac2a6e4c98365ebf6f
A-Little-Hat <soumyasarkar309@gmail.com>
2021-04-19 22:52:43 +0530

    extra files deleted and favicon changed

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,12 +3,12 @@ import TodoItem from './TodoItem'
 
 export default function Todos(props) {
     const todostyle={
-        minHeight:"50vh"
+        minHeight:"40vh"
     }
     return (
         <div className="container my-4" style={todostyle}>
             <hr/>
-            <h3 className="text-center my-3">Todos List</h3>
+            <h3 className="text-center my-3">TodoList</h3>
             {
                 props.todolist.map((todo)=>{
                     return <TodoItem delete={props.delete.bind(todo)} key={todo.no} todo={todo}/>
