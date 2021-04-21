905843728e2b71055685931c0eccc6be190398fe
A-Little-Hat <soumyasarkar309@gmail.com>
2021-04-21 18:37:42 +0530

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer className="bg-dark text-light">
-           <p className="text-center"> Copyright &copy; A-Little-Hat </p>
+           <p className="text-center"> Copyright &copy; A-Little-Hat 2021</p>
         </footer>
     )
 }
