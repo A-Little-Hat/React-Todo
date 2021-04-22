e25c5863b5e57b8771157ff8380a53d0f624fe26
A-Little-Hat <soumyasarkar309@gmail.com>
2021-04-22 13:32:15 +0530

    about updted

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 const About = () => {
     return (
-        <div>
-            about works.!
+        <div className="container my-4" style={{minHeight:"70vh"}}>
+            <p> This Application is build using <b>REACT</b> by <Link to="https://github.com/A-Little-Hat">A-Little-Hat</Link>.</p>
+
+            <p>Visit <Link to="https://soumyaneel.web.app/">Portfolio</Link> </p>
+
+            <p>If you have any issues using this Application, please create a <Link to="https://github.com/A-Little-Hat/React-Todo/issues">Issue</Link></p>
+
+            <p>Contact me on <Link to="https://www.linkedin.com/in/soumyaneel-sarkar-04158a1b0/"><img src="https://image.flaticon.com/icons/png/512/174/174857.png" height="30px" width="30px" alt="" srcset=""/></Link></p>
         </div>
     );
 }
